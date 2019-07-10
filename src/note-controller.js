62a4d2eebc5b0f7ce18601b0a58b62bbaf3772e0
petraartep <petra.pogacsas@gmail.com>
2019-07-10 14:56:42 +0100

    create note thing

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,5 +1,21 @@
-var app = document.getElementById("app");
-console.log(app);
+(function(exports) {
+  
+  function NoteController(noteList) {
+    var noteList = noteList;
 
-app.innerText = "Howdy";
+    function addNote(text) {
+      noteList.createNote("Favourite drink: whisky");
+    }
+    return {
+       addNote: addNote
+    }
+  }
+
+exports.NoteController = NoteController 
+}) (this);
+
+// var app = document.getElementById("app");
+// console.log(app);
+
+// app.innerText = "Howdy";
 
