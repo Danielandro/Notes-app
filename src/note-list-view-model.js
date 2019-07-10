f8ca651998a5d4de5bf245b6279ac3e372ed8480
Danny <mtme13@hotmail.com>
2019-07-10 13:57:02 +0100

    Refactor notelist model

--- src/note-list-view-model.js
+++ src/note-list-view-model.js
@@ -1,22 +1,24 @@
 (function(exports) {
   function NoteListView(noteList) {
-    this.noteList = noteList;
-  };
+    var _noteList = noteList;
 
-  // convert list of notes into html string
-  NoteListView.prototype.convertListToHTML = function() {  
-    var notesToHtml;
-    // loop over array of notes. Create array of html strings 
-    if (this.noteList.allNotes().length === 0) {
-      return "<ul><li>No notes found</li></ul>";
-    } else {
-      var notesToHtml = this.noteList.allNotes().map(function (note) {
-        return `<li><div>${note.text}</div></li>`;
-      });
-    }    
-
-    // return html string, interpolate notes
-    return (`<ul>${notesToHtml.join('')}</ul>`);   
+    function convertListToHTML() {
+      var notesToHtml;
+      // loop over array of notes. Create array of html strings 
+      if (_noteList.allNotes().length === 0) {
+        return "<ul><li>No notes found</li></ul>";
+      } else {
+        var notesToHtml = _noteList.allNotes().map(function (note) {
+          return `<li><div>${note.text}</div></li>`;
+        });
+        // return html string, interpolate notes
+        return (`<ul>${notesToHtml.join('')}</ul>`);
+      };
+    }  
+    
+    return {
+      convertListToHTML: convertListToHTML
+    }
   };
 
   exports.NoteListView = NoteListView;
