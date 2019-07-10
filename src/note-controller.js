607388254e0fac364226ae32f1e70db2f5169fd0
petraartep <petra.pogacsas@gmail.com>
2019-07-10 17:08:59 +0100

    step 6 before testing

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,21 +1,32 @@
 (function(exports) {
   
-  function NoteController(noteList) {
-    var noteList = noteList;
+  function NoteController(noteList) { // Contructor cuz of capital function name 
+    var noteList = noteList; 
+    var noteListView = new NoteListView(noteList); // constructor cuz of new and gets => ();
 
     function addNote(text) {
       noteList.createNote("Favourite drink: whisky");
     }
-    return {
-       addNote: addNote
+
+    function getHTMLToPage() {
+      var getHTMLToPage;
+      
+      var app = document.getElementById("app");
+      console.log(app);
+
+      app.innerText = "Howdy";
+
+      var htmlThing = noteListView.convertListToHTML()  
+
+      app.innerHTML = htmlThing;
+    }
+
+    return { //JS objects , not ;
+       addNote: addNote,
+       getHTMLToPage: getHTMLToPage
     }
   }
 
+
 exports.NoteController = NoteController 
 }) (this);
-
-// var app = document.getElementById("app");
-// console.log(app);
-
-// app.innerText = "Howdy";
-
